9377f834d3813775fed826634a5444e57805d33e
Yohann Andre <y.andre90000@gmail.com>
2023-12-27 15:23:11 +0100

    add icones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,15 @@ const Footer = () => {
       <h2>Made by ANDRE Yohann</h2>
 
       <div className="socials">
-        <a href="https://res.cloudinary.com/dwkwlok28/image/upload/v1703686707/LinkedIn_icon.svg_ywrjtt.png">
+        <a href="https://www.linkedin.com/in/yohann-andr%C3%A9/">
           <img
-            src="src/assets/images/LinkedIn_icon.svg.png"
+            src="https://res.cloudinary.com/dwkwlok28/image/upload/v1703686707/LinkedIn_icon.svg_ywrjtt.png"
             alt="logo linkedin"
           />
         </a>
-        <a href="https://res.cloudinary.com/dwkwlok28/image/upload/v1703686714/github_irxfsl.png">
+        <a href="https://github.com/Nayoh9">
           <img
-            src="src/assets/images/github.png"
+            src="https://res.cloudinary.com/dwkwlok28/image/upload/v1703686714/github_irxfsl.png"
             alt="chat blanc sur fond noir"
           />
         </a>
