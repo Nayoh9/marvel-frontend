fd22bc12386b251f5ac9289970bb878c1334e565
Yohann Andre <y.andre90000@gmail.com>
2023-12-27 15:19:32 +0100

    add icones git linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ const Footer = () => {
       <h2>Made by ANDRE Yohann</h2>
 
       <div className="socials">
-        <a href="https://www.linkedin.com/in/yohann-andr%C3%A9/">
+        <a href="https://res.cloudinary.com/dwkwlok28/image/upload/v1703686707/LinkedIn_icon.svg_ywrjtt.png">
           <img
             src="src/assets/images/LinkedIn_icon.svg.png"
             alt="logo linkedin"
           />
         </a>
-        <a href="https://github.com/Nayoh9">
+        <a href="https://res.cloudinary.com/dwkwlok28/image/upload/v1703686714/github_irxfsl.png">
           <img
             src="src/assets/images/github.png"
             alt="chat blanc sur fond noir"
